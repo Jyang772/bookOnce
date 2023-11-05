db7b7e8d5dda04d30939132086a74b9b13f77053
quaxton <jyang772@gmail.com>
2023-11-05 00:45:04 -0400

    Notification added

--- background.js
+++ background.js
@@ -11,6 +11,14 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
+function getLastQuery(callback) {
+  chrome.storage.local.get(['lastQuery'], function(result) {
+    if (result.lastQuery) {
+      callback(result.lastQuery);
+    }
+  });
+}
+
 function searchBookmarks(query) {
   chrome.bookmarks.search(query, function (results) {
     if (results.length > 0) {
@@ -20,11 +28,27 @@ function searchBookmarks(query) {
 }
 
 function createNotification(query, count) {
-  chrome.notifications.create('', {
+  chrome.storage.local.set({ lastQuery: query }, function() {
+    console.log('Query saved.');
+  });
+
+  chrome.notifications.create('bookmarkNotification', {
     title: 'Bookmark Assistant',
     message: `You have ${count} bookmarks related to: "${query}"`,
     iconUrl: 'images/alert.jpg',
     type: 'basic'
   });
 }
+
+chrome.notifications.onClicked.addListener(function(notificationId) {
+  if (notificationId === 'bookmarkNotification') {
+    //chrome.tabs.create({ url: 'popup.html' }); // Opens the popup.html in a new tab
+	  
+    // Assuming 'query' is a variable that holds the search term
+    //chrome.tabs.create({ url: `chrome://bookmarks/?q=${encodeURIComponent(query)}` });
+    getLastQuery(function(query) {
+      chrome.tabs.create({ url: `chrome://bookmarks/?q=${encodeURIComponent(query)}` });
+    });
+  }
+});
 
